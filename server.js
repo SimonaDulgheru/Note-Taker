280992689c90be6d3610e3094a9a1758fa54950b
Simona <s.c.dulgheru@gmail.com>
2020-01-02 20:23:30 +0000

    code simplified, preview added

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 
 
 const express = require("express");
-const router = require("express").Router();
 const path = require("path");
+const uuid = require("uuid");
 
 const fs = require("fs");
-const store = require("./db/store");
-
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 const db = path.join(__dirname, "./db/db.json");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+// USE
+
+app.use("/assets/css", express.static(path.join(__dirname,"./public/assets/css")));
+
+app.use("/assets/js", express.static(path.join(__dirname,"./public/assets/js")));
+
+app.use(express.static(path.join(__dirname, "./public")));
 
 
 //GET
@@ -33,25 +41,47 @@ app.get("/api/notes", function(req, res) {
     });
   });
 
+  // USE
+
+app.use("/assets/css", express.static(path.join(__dirname,"./public/assets/css")));
+
+app.use("/assets/js", express.static(path.join(__dirname,"./public/assets/js")));
+
+app.use(express.static(path.join(__dirname, "./public")));
 
 
-router.get("/notes", function(req, res) {
-    store.getNotes()
-    .then(notes => res.json(notes))
-    .catch(err => res.status(500).json(err))
-})
+//POST
 
-router.post("/notes", function(req, res){
-    store.addNotes(req.body)
-    .then(note => res.json(note))
-    .catch(err => res.status(500).json(err))
-})
+app.post("/api/notes", function(req,res) {
+    note = {
+        id:uuid.v4(),
+        title:req.body.title,
+        text:req.body.text
+    };
 
-router.delete("/notes/:id", function(req, res) {
-    store.removeNotes(req.params.id)
-    .then(()=>res.json({ok:true}))
-    .catch(err => res.status(500).json(err))
-})
+    fs.readFile(db, (err, data) => {
+        if(err) throw err;
+        const text = JSON.parse(data);
+        text.push(note);
+        fs.writeFile(db, JSON.stringify(text), err => {
+            if (err) throw err;
+        });
+    });
+    res.sendFile(db);
+});
+
+
+app.delete("/api/notes/:id", function(req, res) {
+    fs.readFile(db, (err, data) => {
+        if (err) throw err;
+        const text = JSON.parse(data);
+        const newText = text.filter(file => file.id != req.params.id);
+        fs.writeFile(db, JSON.stringify(newText), err => {
+            if(err) throw err;
+        });
+    });
+    res.sendFile(db);
+});
 
 
 app.listen(PORT, function() {
