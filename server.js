f7a9c78edb73d078b17a4e3ab17cf06ae73c50f9
Simona <s.c.dulgheru@gmail.com>
2020-01-14 13:00:31 +0000

    Save Note added

--- server.js
+++ server.js
@@ -1,77 +1,18 @@
 
-
 const express = require("express");
-const path = require("path");
-const uuid = require("uuid");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-const fs = require("fs");
-
+// Initialize the app and create a port
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-const db = path.join(__dirname, "./db/db.json");
 
+// Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-
-
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html" ));
-})
-
-app.get("/api/notes", function(req, res) {
-    fs.readFile("./db/db.json", (err, data) => {
-      if (err) throw err;
-      return res.json(JSON.parse(data));
-    });
-  });
-
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html" ));
-})
-
- 
-
-app.use("/assets/css", express.static(path.join(__dirname,"./public/assets/css")));
-
-app.use("/assets/js", express.static(path.join(__dirname,"./public/assets/js")));
-
-app.use(express.static(path.join(__dirname, "./public")));
-
-
-app.post("/api/notes", function(req,res) {
-    const note = {
-        id:uuid.v4(),
-        title:req.body.title,
-        text:req.body.text
-    };
-
-    fs.readFile(db, (err, data) => {
-        if(err) throw err;
-        const text = JSON.parse(data);
-        text.push(note);
-        fs.writeFile(db, JSON.stringify(text), err => {
-            if (err) throw err;
-        });
-    });
-    res.sendFile(note);
-});
-
-
-app.delete("/api/notes/:id", function(req, res) {
-    fs.readFile(db, (err, data) => {
-        if (err) throw err;
-        const text = JSON.parse(data);
-        const newText = text.filter(file => file.id != req.params.id);
-        fs.writeFile(db, JSON.stringify(newText), err => {
-            if(err) throw err;
-        });
-    });
-    res.sendFile(db);
-});
-
-
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
