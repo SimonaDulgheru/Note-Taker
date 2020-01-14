e609384f7bcc72edd1b3388c75115480704cdc02
Simona <s.c.dulgheru@gmail.com>
2020-01-14 13:36:56 +0000

    Routes folder modified

--- db/store.js
+++ db/store.js
@@ -21,7 +21,7 @@ class Store {
     return this.read().then(notes => {
       let parsedNotes;
 
-      // If notes isn't an array or can't be turned into one, send back a new empty array
+      
       try {
         parsedNotes = [].concat(JSON.parse(notes));
       } catch (err) {
@@ -39,10 +39,10 @@ class Store {
       throw new Error("Note 'title' and 'text' cannot be blank");
     }
 
-    // Increment `this.lastId` and assign it to `newNote.id`
+   
     const newNote = { title, text, id: ++this.lastId };
 
-    // Get all notes, add the new note, write all the updated notes, return the newNote
+    
     return this.getNotes()
       .then(notes => [...notes, newNote])
       .then(updatedNotes => this.write(updatedNotes))
@@ -50,7 +50,7 @@ class Store {
   }
 
   removeNote(id) {
-    // Get all notes, remove the note with the given id, write the filtered notes
+    
     return this.getNotes()
       .then(notes => notes.filter(note => note.id !== parseInt(id)))
       .then(filteredNotes => this.write(filteredNotes));
