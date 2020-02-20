57a447363413e63b7d3b2910a8d48250355f44a8
Simona <s.c.dulgheru@gmail.com>
2020-02-20 22:20:56 +0000

    Save button working

--- server.js
+++ server.js
@@ -1,19 +1,67 @@
+const fs = require('fs');
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const path = require("path");
+
+
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT||8080;
 
 
-
-
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+app.use(express.json());
+
+app.use(express.static(__dirname + "/public"));
 
 
-app.listen(port, () => console.log(`Listening on PORT ${port}`));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public', 'index.html'));
+});
+
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public', 'notes.html'));
+});
+
+
+app.post("/api/notes", (req, res) => {
+  const rawdata = fs.readFileSync('./db/db.json', 'utf8');
+  const parsedata = JSON.parse(rawdata);
+  const newObj = parsedata.concat(req.body);
+  const string = JSON.stringify(newObj);
+  fs.writeFile('./db/db.json', string, (err)=>{
+    if(err) console.log(err);
+    res.json(string);
+  })
+});
+
+
+app.get('/api/notes', (req,res)=>{
+  fs.readFile('./db/db.json','utf8', (err,jsonString)=>{
+    if(err) {
+      console.log(err);
+      return
+    };
+    res.json(JSON.parse(jsonString))
+  })
+});
+
+
+app.delete('/api/notes/:id', (req,res)=>{
+  const rawData = fs.readFileSync('./db/db.json', 'utf-8');
+  console.log(rawData)
+  const parseData = JSON.parse(rawData);
+  const {id} = req.params.id;
+  console.log(id)
+  const newData = parseData.filter(note => note.id !== id);
+  console.log(newData)
+  fs.writeFile('./db/db.json', JSON.stringify(newData), (err)=>{
+    if(err) console.log(err);
+    res.json(newData);
+  })
+});
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT)});
